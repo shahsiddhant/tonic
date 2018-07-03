5001384f1635eebb50f5c0adcc7bb30433c281e6
shahsiddhant <siddhant@siddhant.design>
2018-07-03 15:53:51 -0700

    added server package.json

---
+++ server/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
+require('./api/models/db');
+require('./api/config/passport');
+
+app.use(passport.initialize());
+app.use('/api', routesApi);
